26b5163be746bd7612310b9a65eb3e8afdaa21e0
Pedro Marchini <pedro-marchini@outlook.com>
2021-09-22 10:10:37 -0300

    Ajustes no projeto

--- Teste Prático/resolucao.js
+++ Teste Prático/resolucao.js
@@ -1,104 +1,108 @@
-const fs = require('fs')
+const fs = require("fs");
+const { setTimeout } = require("timers");
 
-//RESTAURAÇÃO DO BRONKEN-DATABASE.JSON
-//#region 
-
-//Lendo os dados do json corrompido.
-const data = fs.readFileSync('broken-database.json');
-const dataBase = JSON.parse(data);
-
-//Corrigindo os caracteres corrompidos.
-for (var i = 0; i < dataBase.length; i++) {
-  dataBase[i].name = dataBase[i].name.toString().replace(/æ/g, "a");
-  dataBase[i].name = dataBase[i].name.toString().replace(/ß/g, "b");
-  dataBase[i].name = dataBase[i].name.toString().replace(/¢/g, "c");
-  dataBase[i].name = dataBase[i].name.toString().replace(/ø/g, "o");
-
-  if (dataBase[i].price.typeof != "number") {
-    dataBase[i].price = parseFloat(dataBase[i].price);
-  }
-
-  if (dataBase[i].quantity === undefined) {
-    dataBase[i].quantity = 0;
-  }
+function jsonReader(filePath, cb) {
+  fs.readFile(filePath, "utf-8", (err, filePath) => {
+    if (err) {
+      return cb && cb(err);
+    }
+    try {
+      const object = JSON.parse(filePath);
+      return cb && cb(null, object);
+    } catch (err) {
+      return cb && cb(err);
+    }
+  });
 }
 
-//Saída do arquivo restaurado.
-fs.writeFileSync('./saida.json', JSON.stringify(dataBase, null, 2), err => {
+jsonReader("./broken-database.json", (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log("Arquivo gerado com sucesso!");
+    for (let i = 0; i < data.length; i++) {
+      data[i].name = data[i].name.toString().replace(/æ/g, "a");
+      data[i].name = data[i].name.toString().replace(/ß/g, "b");
+      data[i].name = data[i].name.toString().replace(/¢/g, "c");
+      data[i].name = data[i].name.toString().replace(/ø/g, "o");
+
+      if (data[i].price.typeof != "number") {
+        data[i].price = parseFloat(data[i].price);
+      }
+
+      if (data[i].quantity === undefined) {
+        data[i].quantity = 0;
+      }
+    }
+    fs.writeFile("./saida.json", JSON.stringify(data, null, 2), (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
-})
-//#endregion
+});
 
-//CHECANDO CORREÇÕES E INCLUINDO FUNÇÕES DE SOMA.
-//#region
-//Definindo as váriaveis (s = soma ,sQ = soma quantia, l = lista).
-var sQEletrodomesticos = 0;
-var sQEletronicos = 0;
-var sQPanelas = 0;
-var sQAcessorios = 0;
+setTimeout(() => {
+  jsonReader("./saida.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      let sQEletrodomesticos = 0;
+      let sQEletronicos = 0;
+      let sQPanelas = 0;
+      let sQAcessorios = 0;
 
-var sEletrodomesticos = 0;
-var sEletronicos = 0;
-var sPanelas = 0;
-var sAcessorios = 0;
+      let sEletrodomesticos = 0;
+      let sEletronicos = 0;
+      let sPanelas = 0;
+      let sAcessorios = 0;
 
-var lista = [];
+      let lista = [];
 
-//Lendo os dados do json restaurado.
-const restoredData = fs.readFileSync('./saida.json');
-const restoredDataBase = JSON.parse(restoredData);
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].category === "Acessórios") {
+          sQAcessorios += data[i].quantity;
+          sAcessorios += data[i].price * data[i].quantity;
+          lista[i] = [data[i].category, data[i].id, data[i].name];
+        } else if (data[i].category === "Eletrodomésticos") {
+          sEletrodomesticos += data[i].price * data[i].quantity;
+          sQEletrodomesticos += data[i].quantity;
+          lista[i] = [data[i].category, data[i].id, data[i].name];
+        } else if (data[i].category === "Eletrônicos") {
+          sQEletronicos += data[i].quantity;
+          sEletronicos += data[i].price * data[i].quantity;
+          lista[i] = [data[i].category, data[i].id, data[i].name];
+        } else {
+          sQPanelas += data[i].quantity;
+          sPanelas += data[i].price * data[i].quantity;
+          lista[i] = [data[i].category, data[i].id, data[i].name];
+        }
+      }
+      console.log(lista.sort());
 
-//Retorno da lista com os nomes dos produtos.
+      //Espaçamento
+      console.log("-----------------------------------------");
 
-//Somas da quantidade de estoque por categoria, valor total e criação da lista com categora, id e nome.
-for (var i = 0; i < restoredDataBase.length; i++) {
-  if (restoredDataBase[i].category === "Acessórios") {
-    sQAcessorios += restoredDataBase[i].quantity;
-    sAcessorios += restoredDataBase[i].price * restoredDataBase[i].quantity
-    lista[i] = [restoredDataBase[i].category, restoredDataBase[i].id, restoredDataBase[i].name];
-  }
-  else if (restoredDataBase[i].category === "Eletrodomésticos") {
-    sEletrodomesticos += restoredDataBase[i].price * restoredDataBase[i].quantity;
-    sQEletrodomesticos += restoredDataBase[i].quantity;
-    lista[i] = [restoredDataBase[i].category, restoredDataBase[i].id, restoredDataBase[i].name];
-  }
-  else if (restoredDataBase[i].category === "Eletrônicos") {
-    sQEletronicos += restoredDataBase[i].quantity;
-    sEletronicos += restoredDataBase[i].price * restoredDataBase[i].quantity
-    lista[i] = [restoredDataBase[i].category, restoredDataBase[i].id, restoredDataBase[i].name];
-  } else {
-    sQPanelas += restoredDataBase[i].quantity;
-    sPanelas += restoredDataBase[i].price * restoredDataBase[i].quantity
-    lista[i] = [restoredDataBase[i].category, restoredDataBase[i].id, restoredDataBase[i].name];
-  }
-}
+      //Deixar a variável "sEletrodomesticos" com apenas duas casas decimais.
+      let fixDecimal = parseFloat(sEletrodomesticos.toFixed(2));
 
-console.log(lista.sort());
+      //Retorno da quantia total de estoque por categoria.
+      console.log("Soma dos estoques por categoria");
+      console.log("Soma do estoque de Acessórios: " + sQAcessorios);
+      console.log("Soma do estoque de Eletrodomésticos: " + sQEletrodomesticos);
+      console.log("Soma do estoque de Eletrônicos: " + sQEletronicos);
+      console.log("Soma do estoque de Panelas: " + sQPanelas);
 
-//Espaçamento
-console.log("-----------------------------------------");
+      //Espaçamento
+      console.log("-----------------------------------------");
 
-//Deixar a variável "sEletrodomesticos" com apenas duas casas decimais.
-var fixDecimal = parseFloat(sEletrodomesticos.toFixed(2));
-
-//Retorno da quantia total de estoque por categoria.
-console.log("Soma dos estoques por categoria")
-console.log("Soma do estoque de Acessórios: " + sQAcessorios);
-console.log("Soma do estoque de Eletrodomésticos: " + sQEletrodomesticos);
-console.log("Soma do estoque de Eletrônicos: " + sQEletronicos);
-console.log("Soma do estoque de Panelas: " + sQPanelas);
-
-//Espaçamento
-console.log("-----------------------------------------");
-
-//Retorno da soma total dos valores de cada uma das categorias, separado por categoria.
-console.log("Soma do total dos valores")
-console.log("Soma do total dos valores de Acessórios: " + sAcessorios);
-console.log("Soma do total dos valores de Eletrodomésticos: " + fixDecimal);
-console.log("Soma do total dos valores de Eletrônicos: " + sEletronicos);
-console.log("Soma do total dos valores de Panelas: " + sPanelas);
-//#endregion
+      //Retorno da soma total dos valores de cada uma das categorias, separado por categoria.
+      console.log("Soma do total dos valores");
+      console.log("Soma do total dos valores de Acessórios: " + sAcessorios);
+      console.log(
+        "Soma do total dos valores de Eletrodomésticos: " + fixDecimal
+      );
+      console.log("Soma do total dos valores de Eletrônicos: " + sEletronicos);
+      console.log("Soma do total dos valores de Panelas: " + sPanelas);
+    }
+  });
+});
